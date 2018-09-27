179591d4276e21c08bf09aa85401969960c7cbdc
Доліч Денис Олегович <gavneco17@gmail.com>
2018-09-27 17:00:37 +0200

    foreach example

---
+++ Scripts/myScript.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $("#getComments").click(function () {
+        viewModel.comments([]);
+        $.get('/api/values', function (data) {
+            viewModel.comments(data);
+        });
+
+    });
+});
